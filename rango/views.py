ccc149baaad8df48efc4b78faca6bb5f27353310
Jinyi Li <2521896l@student.gla.ac.uk>
2022-01-27 12:59:52 +0000

    Add chapter 4 codes

--- rango/views.py
+++ rango/views.py
@@ -1,15 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def about(request):
-    response = HttpResponse()
-    response.write("Rango says here is the about page.")
-    response.write("<a href='/rango/'>Index</a>")
-    return response
+    # response = HttpResponse()
+    # response.write("Rango says here is the about page.")
+    # response.write("<a href='/rango/'>Index</a>")
+    return render(request, 'rango/about.html')
 
 
 def index(request):
-    response = HttpResponse()
-    response.write("Rango says hey there partner!")
-    response.write("<a href='/rango/about/'>About</a>")
-    return response
+    # response = HttpResponse()
+    # response.write("Rango says hey there partner!")
+    # response.write("<a href='/rango/about/'>About</a>")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+
+    return render(request, 'rango/index.html', context=context_dict)
--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -63,6 +63,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.media',
             ],
         },
     },
@@ -119,3 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+MEDIA_DIR = os.path.join(BASE_DIR, 'media')
+
+MEDIA_ROOT = MEDIA_DIR
+
+MEDIA_URL = '/media/'
+
+TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
